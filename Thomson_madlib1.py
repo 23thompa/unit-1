f5c9040e852b94ad0bff3f53ce2398f437436cd5
23thompa <59451311+23thompa@users.noreply.github.com>
2020-01-10 12:06:46 -0500

    Add files via upload

---
+++ Thomson_madlib1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#Madlib 1
+
+print("Let's Play Mad Libs!")
+print("")
+name = input("Enter a name: ")
+adjectiveone = input("Enter an adjective: ")
+adjectivetwo = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+foodone = input("Enter a food: ")
+foodtwo = input("Enter another food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+print("")
+
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking foward to trying the local \ncuisine, including " + adjectiveone,foodone + " and " + foodtwo + ".")
+print("")
+print(name + " will have to practice the language " + adverb + " to \nmake it easier to " + verb + " with people.")
+print("")
+print(name + " has a long list of sights to see, including the \n" + noun + " museum and the " + adjectivetwo + " park.")
+
---
+++ Thomson_unit1_practice.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# PART A
+
+#Number Two/One
+
+print("Welcome to Information Technology\n\nProgramming and\nWeb Developnent")
+print("")
+print(" _       ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+
+print("")
+print("")
+print("")
+
+# PART B
+
+#Number One
+
+print("A lighting flash:\nbetween the forest trees\nI have seen water.\n\t- Shiki")
+
+print("")
+print("")
+
+#Number Two
+
+print("**********\n*        *\n*  MR.G  *\n* RM 119 *\n*        *\n**********")
+
+print("")
+print("")
+
+#Number Three
+
+print("\"Computer Science is no more about computers\nthan astronomy is about telescopes.\"\n- Edsger W. Dijkstra")
+
+print("")
+print("")
+print("")
+
+#Number Four
+
+print ("            )\n           (\n       ^    )\n      / \ ---\n     /   \| |\n    /  -  \ |\n   /  (_)  \|\n  /         \ \n /|  _   _  |\ \n  | | | | | | \ \n  | |_| |_| |  \n  |  __  __ |\n  | | | | | |      #\n  | |@| |_| |     ###\n  | | |     |    ####\n\/\/\/\/\/\/\/\/\/\/\/\/\/")
+
+#PART C
+
+#Number One
+
+
+#Part E
+
+a = input("Enter an animal: ")
+s = input("Enter a sound: ")
+e = ("E-I-E-I-O")
+
+print("Old Macdonald had a farm, " + e)
+print("And on his farm he had a " + a + ", " + e)
+print("With a " + s + "-" + s + " here and a " + s + "-" + s + " there")
+print("Here a " + s + " there a " + s)
+print("Everywhere a " + s + "-" + s)
+print("Old Macdonald had a farm, " + e)
